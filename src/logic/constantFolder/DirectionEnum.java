baad6aae4c7caa5bbe0fc768f62a61daf19c2f1d
RouRai <k1215050@students.katyisd.org>
2023-03-27 17:30:31 -0500

    BIG ENUM CAHNGES

--- src/logic/constantFolder/DirectionEnum.java
+++ src/logic/constantFolder/DirectionEnum.java
@@ -1,6 +1,6 @@
 package logic.constantFolder;
 
 public enum DirectionEnum {
-    TOP_LEFT, TOP, TOP_RIGHT, BOTTOM_RIGHT, BOTTOM, BOTTOM_LEFT
+    TOP_LEFT, TOP_RIGHT, LEFT, RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT
 
 }
