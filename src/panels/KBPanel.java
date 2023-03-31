a72d450b3ec209144ab2f6fca2f3ae017e847606
JessCai06 <jesscai006@gmail.com>
2023-03-31 16:49:24 -0500

    modularized graphics boards and buttons by changing to  button Quadrants

--- src/panels/KBPanel.java
+++ src/panels/KBPanel.java
@@ -1,5 +1,6 @@
 package panels;
 
+import custom.ButtonQuadrant;
 import custom.HexagonButton;
 import game.Constants;
 //import hexxes.hexmech;
@@ -16,31 +17,29 @@ public class KBPanel extends JPanel implements ActionListener {
    //Images
 
    public BufferedImage background, highlight;
+   public ButtonQuadrant[] boards;
    public Graphics2D g2;
-   private CardLayout cl;
+   private CardLayout cardLay;
    private HexagonButton[][] board, board2, board3, board4;
 
-   public KBPanel (CardLayout cardL){
+   public KBPanel (CardLayout cl){
       //setLayout(null);
-      cl = cardL;
+      cardLay = cl;
 
-      board = new HexagonButton[10][10];
-      board2 = new HexagonButton[10][10];
-      board3 = new HexagonButton[10][10];
-      board4 = new HexagonButton[10][10];
-      for(int r = 0; r < 10; r++){
-         for(int c = 0; c < 10; c++){
+      //Boards setup - see ButtonQuadrant class for more details
+      boards = new ButtonQuadrant[4];
+      int[] boardStartX = {10,423,10,423};
+      int[] boardStartY = {6,6,365,365};
 
-            board[r][c] = new HexagonButton(1,r,c);
-            setUpHexes(board[r][c]);
-            board2[r][c] = new HexagonButton(2,r,c);
-            board3[r][c] = new HexagonButton(3,r,c);
-            board4[r][c] = new HexagonButton(4,r,c);
-
-            setUpHexes(board2[r][c]);
-            setUpHexes(board3[r][c]);
-            setUpHexes(board4[r][c]);
+      for (int q = 0; q < 4; q++) {
+         HexagonButton[][] tempBoard = new HexagonButton[10][10];
+         for (int r = 0; r < 10; r++) {
+            for (int c = 0; c < 10; c++) {
+               tempBoard[r][c] = new HexagonButton(q, r, c);
+               setUpHexes(tempBoard[r][c]);
+            }
          }
+         boards[q] = new ButtonQuadrant(q,tempBoard, boardStartX[q],boardStartY[q]);
       }
       try{
          // 1 -- BACKGROUND - BOTTOM LAYER
@@ -54,91 +53,47 @@ public class KBPanel extends JPanel implements ActionListener {
    public void paintComponent(Graphics g)
    {
       g2 = (Graphics2D)g;
-
+      //Base Calls
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
       g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
       super.paintComponent(g2);
       // 1 -- BACKGROUND
       g2.drawImage(background,0, 0, Constants.WIDTH, Constants.HEIGHT-8, null);
       g2.setBackground(Color.BLACK);
-      //g2.fillRect(0,0,2000,1000);
-     // g2.drawImage(boards[0], 0, 0,400, 400, null);
-      //top right board
-      double x = 423;
-      double y = 6;
-      for(int r = 0; r < 10; r++){
-         for(int c = 0; c < 10; c++){
-            if(r % 2 == 0){
-               board[r][c].setColor(Color.RED);
-               board[r][c].setBounds((int)(x + c * 41.2), (int)y, 46, 46);
-            } else{
-               board[r][c].setColor(Color.yellow);
-               board[r][c].setBounds((int)(x + 21 + c * 41.3), (int)y, 46, 46);
-            }
-            g2.drawImage(highlight, board[r][c].getX() - 40, board[r][c].getY() - 35, 120, 120, null);
-            //board[r][c].setIcon(highlight);
-            //gameButton.setIcon(icon);
-         }
-         y+=35.5;
-      }
-      //top left board
-      x = 10;
-      y = 6;
-      for(int r = 0; r < 10; r++){
-         for(int c = 0; c < 10; c++){
-            if(r % 2 == 0){
-               board2[r][c].setColor(Color.RED);
-               board2[r][c].setBounds((int)(x + c * 41.2), (int)y, 46, 46);
-            } else{
-               board2[r][c].setColor(Color.yellow);
-               board2[r][c].setBounds((int)(x + 21 + c * 41.3), (int)y, 46, 46);
-            }
-            g2.drawImage(highlight, board2[r][c].getX() - 40, board2[r][c].getY() - 35, 120, 120, null);
-            //board[r][c].setIcon(highlight);
-            //gameButton.setIcon(icon);
-         }
-         y+=35.5;
-      }
-      //bottom left board
-      x = 10;
-      y = 365;
-      for(int r = 0; r < 10; r++){
-         for(int c = 0; c < 10; c++){
-            if(r % 2 == 0){
-               board3[r][c].setColor(Color.RED);
-               board3[r][c].setBounds((int)(x + c * 41.2), (int)y, 46, 46);
-            } else{
-               board3[r][c].setColor(Color.yellow);
-               board3[r][c].setBounds((int)(x + 21 + c * 41.3), (int)y, 46, 46);
-            }
-            g2.drawImage(highlight, board3[r][c].getX() - 40, board3[r][c].getY() - 35, 120, 120, null);
-            //board[r][c].setIcon(highlight);
-            //gameButton.setIcon(icon);
-         }
-         y+=35.5;
-      }
-      //bottom right board
-      x = 423;
-      y = 365;
-      for(int r = 0; r < 10; r++){
-         for(int c = 0; c < 10; c++){
-            if(r % 2 == 0){
-               board4[r][c].setColor(Color.RED);
-               board4[r][c].setBounds((int)(x + c * 41.2), (int)y, 46, 46);
-            } else{
-               board4[r][c].setColor(Color.yellow);
-               board4[r][c].setBounds((int)(x + 21 + c * 41.3), (int)y, 46, 46);
-            }
-            g2.drawImage(highlight, board4[r][c].getX() - 40, board4[r][c].getY() - 35, 120, 120, null);
-            //board[r][c].setIcon(highlight);
-            //gameButton.setIcon(icon);
-         }
-         y+=35.5;
-      }
-      g2.setColor(new Color (0,0,0,50));
-
+     //g2.drawImage(Constants.getBoards()[0], 0, 0,400, 400, null);
+     drawBoards();
 
    }
+
+   /**
+    * draws the outline for each Hexbutton with Button Quadrant
+    */
+   public void drawBoards(){
+      for (ButtonQuadrant b: boards) {
+         double x = b.startX;
+         double y = b.startY;
+         HexagonButton [][] board = b.getBoard();
+         for (int r = 0; r < 10; r++) {
+            for (int c = 0; c < 10; c++) {
+               if (r % 2 == 0) {
+                  board[r][c].setColor(Color.RED);
+                  board[r][c].setBounds((int) (x + c * 41.2), (int) y, 46, 46);
+               } else {
+                  board[r][c].setColor(Color.yellow);
+                  board[r][c].setBounds((int) (x + 21 + c * 41.3), (int) y, 46, 46);
+               }
+               g2.drawImage(highlight, board[r][c].getX() - 40, board[r][c].getY() - 35, 120, 120, null);
+               //board[r][c].setIcon(highlight);
+               //gameButton.setIcon(icon);
+            }
+            y += 35.5;
+         }
+      }
+   }
+   /**
+    * @param temp hexbutton
+    *             send in a hex button to set up its action listener function
+    */
    public void setUpHexes(HexagonButton temp) {
       add(temp);
       // cl.show(Constants.PANEL_CONT, Constants.GAME_PANEL);
