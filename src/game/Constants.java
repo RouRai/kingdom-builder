b214b1e54c4fcfd0441bc5f9793f6f7a6e92b756
JessCai06 <jesscai006@gmail.com>
2023-04-01 11:32:19 -0500

    set up current player static layout - rough coordinates - subject to change

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -5,7 +5,7 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 public class Constants {
     // Images
-        public static BufferedImage[] boards, flippedBoards, actionTiles, charCards, landCards, settlements;
+        public static BufferedImage[] boards, flippedBoards, actionTiles, charCards, landCards, settlements, actionProcess;
     // Screen dimensions
          public static final int WIDTH = 1536;
         public static final int HEIGHT = 864;
@@ -22,10 +22,23 @@ public class Constants {
         boards = new BufferedImage [8];
         flippedBoards = new BufferedImage [16];
         settlements = new BufferedImage [4];
-        landCards = new BufferedImage [5];
+        landCards = new BufferedImage [6];
         charCards = new BufferedImage [10];
         actionTiles = new BufferedImage[8];
+        actionProcess = new BufferedImage[8];
 
+        try{
+            // 2 -- BOARDS
+            actionProcess[0] = ImageIO.read(getClass().getResource("/images/actionProcess/Oasis.png"));
+            actionProcess[1] = ImageIO.read(getClass().getResource("/images/actionProcess/Boat.png"));
+            actionProcess[2] = ImageIO.read(getClass().getResource("/images/actionProcess/Farm.png"));
+            actionProcess[3] = ImageIO.read(getClass().getResource("/images/actionProcess/Horse.png"));
+            actionProcess[4] = ImageIO.read(getClass().getResource("/images/actionProcess/Barn.png"));
+            actionProcess[5] = ImageIO.read(getClass().getResource("/images/actionProcess/Oracle.png"));
+            actionProcess[6] = ImageIO.read(getClass().getResource("/images/actionProcess/Tower.png"));
+            actionProcess[7] = ImageIO.read(getClass().getResource("/images/actionProcess/Tavern.png"));
+        } catch (Exception ex) {
+            System.out.println(" actionProcess error");}
         try{
             // 2 -- BOARDS
             boards[0] = ImageIO.read(getClass().getResource("/images/boards/beach.png"));
@@ -36,21 +49,33 @@ public class Constants {
             boards[5] = ImageIO.read(getClass().getResource("/images/boards/stone.png"));
             boards[6] = ImageIO.read(getClass().getResource("/images/boards/tower.png"));
             boards[7] = ImageIO.read(getClass().getResource("/images/boards/Tavern.png"));
+        } catch (Exception ex) {
+            System.out.println("board error");}
+        try{
             // 2.5 -- FLIPPED BOARDS
             boards[0] = ImageIO.read(getClass().getResource("/images/boards/beach_flipped.png"));
             boards[1] = ImageIO.read(getClass().getResource("/images/boards/boat_flipped.png"));
             boards[2] = ImageIO.read(getClass().getResource("/images/boards/farm_flipped.png"));
-            boards[3] = ImageIO.read(getClass().getResource("/images/boards/horse_flipped.png"));
+            boards[3] = ImageIO.read(getClass().getResource("/images/boards/paddock_flipped.png"));
             boards[4] = ImageIO.read(getClass().getResource("/images/boards/house_flipped.png"));
             boards[5] = ImageIO.read(getClass().getResource("/images/boards/oracle_flipped.png"));
             boards[6] = ImageIO.read(getClass().getResource("/images/boards/tower_flipped.png"));
             boards[7] = ImageIO.read(getClass().getResource("/images/boards/tavern_flipped.png"));
-            // 3 -- SETTLEMENTS
+            } catch (Exception ex1) {
+                System.out.println("board flipped error");}
+
+            try{
+                // 3 -- SETTLEMENTS
             settlements[0] = ImageIO.read(getClass().getResource("/images/settlementIcons/Black_Settlement - Copy.png"));
             settlements[1] = ImageIO.read(getClass().getResource("/images/settlementIcons/Blue_Settlement - Copy.png"));
             settlements[2] = ImageIO.read(getClass().getResource("/images/settlementIcons/Red_Settlement - Copy.png"));
             settlements[3] = ImageIO.read(getClass().getResource("/images/settlementIcons/White_Settlement - Copy.png"));
-            // 4 - ACTIONTILES
+                }
+            catch (Exception ex2) {
+                    System.out.println("settlement error");}
+
+            try{
+                // 4 - ACTIONTILES
             actionTiles[0] = ImageIO.read(getClass().getResource("/images/actionTiles/barn_Tile.png"));
             actionTiles[1] = ImageIO.read(getClass().getResource("/images/actionTiles/farm_Tile.png"));
             actionTiles[2] = ImageIO.read(getClass().getResource("/images/actionTiles/harbor_Tile.png"));
@@ -59,6 +84,10 @@ public class Constants {
             actionTiles[5] = ImageIO.read(getClass().getResource("/images/actionTiles/paddock_Tile.png"));
             actionTiles[6] = ImageIO.read(getClass().getResource("/images/actionTiles/tavern_Tile.png"));
             actionTiles[7] = ImageIO.read(getClass().getResource("/images/actionTiles/tower_Tile.png"));
+                    }
+            catch (Exception ex3) {
+                        System.out.println("actionTile error");}
+        try{
             // 4 - charCards
             charCards[0] = ImageIO.read(getClass().getResource("/images/characterCards/Citizen.png"));
             charCards[1] = ImageIO.read(getClass().getResource("/images/characterCards/Discoverers.png"));
@@ -68,17 +97,20 @@ public class Constants {
             charCards[5] = ImageIO.read(getClass().getResource("/images/characterCards/Marchants.png"));
             charCards[6] = ImageIO.read(getClass().getResource("/images/characterCards/Miners.png"));
             charCards[7] = ImageIO.read(getClass().getResource("/images/characterCards/Workers.png"));
-            charCards[8] = ImageIO.read(getClass().getResource("/images/characterCards/Lord.png"));
+            charCards[8] = ImageIO.read(getClass().getResource("/images/characterCards/Lords.png"));
             charCards[9] = ImageIO.read(getClass().getResource("/images/characterCards/Hermits.png"));
-            //5- landCards
-            landCards[0] = ImageIO.read(getClass().getResource("/images/landscapeCards/KB-Card-Back.png"));
+                        }
+        catch (Exception ex4) {
+                            System.out.println("char card error");}
+                    try{  //5- landCards
+            landCards[5] = ImageIO.read(getClass().getResource("/images/landscapeCards/KB-Card-Back.png"));
             landCards[1] = ImageIO.read(getClass().getResource("/images/landscapeCards/KB-Card-Canyon.png"));
             landCards[2] = ImageIO.read(getClass().getResource("/images/landscapeCards/KB-Card-Desert.png"));
             landCards[3] = ImageIO.read(getClass().getResource("/images/landscapeCards/KB-Card-Flower.png"));
             landCards[4] = ImageIO.read(getClass().getResource("/images/landscapeCards/KB-Card-Forest.png"));
-            landCards[5] = ImageIO.read(getClass().getResource("/images/landscapeCards/KB-Card-Meadow.png"));
-        } catch (Exception ex) {
-            System.out.println("----------------------------------------- Image Error -----------------------------------------");
+            landCards[0] = ImageIO.read(getClass().getResource("/images/landscapeCards/KB-Card-Meadow.png"));
+        } catch (Exception ex5) {
+            System.out.println("landscape error");
         }
     }
     public static BufferedImage getImage(String name) {
@@ -108,5 +140,9 @@ public class Constants {
 
     public static BufferedImage[] getSettlements() {
         return settlements;
+    }
+
+    public static BufferedImage[] getActionProcess() {
+        return actionProcess;
     }
 }
--- src/panels/KBPanel.java
+++ src/panels/KBPanel.java
@@ -12,7 +12,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class KBPanel extends JPanel implements ActionListener {
    //Images
 
@@ -21,12 +29,19 @@ public class KBPanel extends JPanel implements ActionListener {
    private Graphics2D g2;
    private ArrayList <BufferedImage> boardImages;
    private CardLayout cardLay;
-   private HexagonButton[][] board, board2, board3, board4;
+   private Constants constantClass;
 
    public KBPanel (CardLayout cl){
       //setLayout(null);
       cardLay = cl;
+      constantClass = new Constants();
 
+      // for coordinates
+      addMouseListener(new MouseAdapter() {
+         @Override
+         public void mousePressed(MouseEvent e) {
+            System.out.println("mouse clicked on coord (" +e.getX()+ ", " +e.getY()+ ")");
+         }});
       //Boards setup - see ButtonQuadrant class for more details
       boards = new ButtonQuadrant[4];
       int[] boardStartX = {10,423,10,423};
@@ -57,16 +72,51 @@ public class KBPanel extends JPanel implements ActionListener {
       g2 = (Graphics2D)g;
       //Base Calls
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-      g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
       super.paintComponent(g2);
       // 1 -- BACKGROUND
       g2.drawImage(background,0, 0, Constants.WIDTH, Constants.HEIGHT-8, null);
       g2.setBackground(Color.BLACK);
      //g2.drawImage(Constants.getBoards()[0], 0, 0,400, 400, null);
 
-      // 2- drawBoards
-     drawBoards();
+      //DRAW BUTTONS
+         drawBoards();
+         drawCurrentPlayer();
+      //
 
+   }
+
+   public void drawCurrentPlayer(){
+      //Player p = null;
+      //number
+      g2.setColor(Color.black);
+      g2.setFont(new Font("TimesRoman", Font.PLAIN, 40));
+      g2.drawString("1",1190,460);
+      //action tiles
+      g2.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+      g2.setColor(Color.white);
+            for (int i = 0; i < 4; i++) {
+               if (i % 2== 0) {
+                  g2.drawImage(constantClass.getActionTiles()[0], 1005, 515+ i * 75, 80, 80 , null);
+                  g2.drawString("0",980,560+ i * 75);
+               }
+               else{
+                  g2.drawImage(constantClass.getActionTiles()[0], 960, 515+ i * 75, 80, 80 , null);
+                  g2.drawString("0",1055,560+ i * 75);
+               }
+            }
+      //action tile selected
+      if (true){
+         g2.drawImage(constantClass.getActionProcess()[6], 1135, 645, 150, 65, null);
+      }
+      //landscape card
+         g2.drawImage(constantClass.getLandCards()[0], 1335, 530, 130, 200, null);
+
+      //settlement
+         //settlement icon
+            g2.drawImage(constantClass.getSettlements()[0], 1330, 420, 120, 80, null);
+         //settlement number
+            g2.setFont(new Font("TimesRoman", Font.PLAIN, 50));
+            g2.drawString("40",1365,480);
    }
 
    /**
@@ -79,11 +129,12 @@ public class KBPanel extends JPanel implements ActionListener {
          HexagonButton [][] board = b.getBoard();
          for (int r = 0; r < 10; r++) {
             for (int c = 0; c < 10; c++) {
+               //CONDITION IF HEX IS ENABLED IN MATRIX.
                if (r % 2 == 0) {
-                  board[r][c].setColor(Color.RED);
+                  //board[r][c].setColor(Color.RED);
                   board[r][c].setBounds((int) (x + c * 41.2), (int) y, 46, 46);
                } else {
-                  board[r][c].setColor(Color.yellow);
+                  //board[r][c].setColor(Color.yellow);
                   board[r][c].setBounds((int) (x + 21 + c * 41.3), (int) y, 46, 46);
                }
                g2.drawImage(highlight, board[r][c].getX() - 40, board[r][c].getY() - 35, 120, 120, null);
