aad233eb608cf58913fc483bee555df6017542b7
RouRai <k1215050@students.katyisd.org>
2023-04-14 17:17:17 -0500

    javadoc additions

--- src/files/QuadrantMaker.java
+++ src/files/QuadrantMaker.java
@@ -64,27 +64,36 @@ public class QuadrantMaker {
       }
    }
 
+   /**
+    * Sets up the enum matrix given a board number.
+    * @param boardNumber The number of a corresponding board.
+    */
    public void setUpEnumMatrix(int boardNumber) {
       try {
          String fileURL = Objects.requireNonNull(getClass().getResource("/files/textFiles/" + boardNames[boardNumber] + "")).getFile();
          File myObj = new File(fileURL);
          Scanner myReader = new Scanner(myObj);
-         //if(this.boardNumber < Constants.getBoards().length)
-            createTerrainMatrix(enumTiles, myReader);
-         //else
-            //createFlippedTerrainMatrix(enumTiles, myReader);
+         createTerrainMatrix(enumTiles, myReader);
       }catch(FileNotFoundException e){
+         e.printStackTrace();
+      }catch(IndexOutOfBoundsException e) {
+         System.out.println("The board number given is out of bounds.");
          e.printStackTrace();
       }
    }
 
-   private void createTerrainMatrix (TerrainEnum[][] terrainMatrix, Scanner myReader) {
+   /**
+    * Creates the TerrainEnum Matrix given an empty TerrainEnum matrix and a Scanner to be used.
+    * @param terrainMatrix The empty matrix to be modified to store the board.
+    * @param fileReader The Scanner object to be used in order to read the file.
+    */
+   private void createTerrainMatrix (TerrainEnum[][] terrainMatrix, Scanner fileReader) {
       int rows = 0;
-      while (myReader.hasNext()) {
-         String data = myReader.nextLine();
-         String[] arr = data.split(" ");
+      while (fileReader.hasNext()) {
+         String data = fileReader.nextLine();
+         String[] symbols = data.split(" ");
          int columns = 0;
-         for (String symbol: arr){
+         for (String symbol: symbols){
             terrainMatrix[rows][columns] = getTerrainTypeFromSymbol(symbol);
             columns++;
          }
@@ -92,6 +101,9 @@ public class QuadrantMaker {
       }
    }
 
+   /**
+    * Flips the enumTiles matrix instance variable for flipped boards.
+    */
    private void flipTerrainMatrix(){
       System.out.println(Arrays.deepToString(enumTiles));
       TerrainEnum[][] flippedBoard = new TerrainEnum[10][10];
@@ -102,6 +114,11 @@ public class QuadrantMaker {
       System.out.println(Arrays.deepToString(enumTiles));
    }
 
+   /**
+    * Returns the corresponding TerrainEnum to a symbol.
+    * @param symbol The symbol given in a text file.
+    * @return TerrainEnum that corresponds to the symbol.
+    */
    private TerrainEnum getTerrainTypeFromSymbol (String symbol) {
       return switch (symbol) {
          case "d" -> TerrainEnum.DESERT;
