2ae839d3a12abd3d707dc0ba4b78cab09c10db41
RouRai <k1215050@students.katyisd.org>
2023-04-14 18:30:43 -0500

    some constructor changes

--- src/datastructures/Main.java
+++ src/datastructures/Main.java
@@ -15,19 +15,5 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<>();
-        names.add("tavern"); names.add("farm"); names.add("house"); names.add("oracle");
-        BoardMatrix bm = new BoardMatrix(names);
-        TerrainNode[][] boardMatrix = bm.getBoardMatrix();
-        for (TerrainNode[] matrix : boardMatrix) {
-            for (TerrainNode terrainNode : matrix) {
-                System.out.print(terrainNode.getType() + " ");
-            }
-            System.out.println();
-        }
-        HashMap map = boardMatrix[0][0].getAdjacentNodes();
-        TerrainNode node = (TerrainNode) map.get(DirectionEnum.RIGHT);
-        System.out.println(node.getType());
-        System.out.println(map.get(DirectionEnum.LEFT));
     }
 }
